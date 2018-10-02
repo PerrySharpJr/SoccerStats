69cbce98ccdfdabccc4885aecdf1eb20224a489c
perrysharpjr <perrysharpjr@gmail.com>
2018-10-01 20:38:38 -0400

    Working with Enums

--- SoccerStats/SoccerStats/GameResults.cs
+++ SoccerStats/SoccerStats/GameResults.cs
@@ -8,6 +8,14 @@ namespace SoccerStats
 {
     public class GameResult
     {
-        public DateTime GameDate { get; set; 5}
+        public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+    }
+
+    public enum HomeOrAway
+    {
+        Home,
+        Away
     }
 }
